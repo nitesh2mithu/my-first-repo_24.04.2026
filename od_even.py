ae17721c0c4c06f3e658c3395fc7a49796b865a7
nitesh2mithu <nitesh2mithu@gmail.com>
2026-04-25 02:35:09 +0530

    od_even.py

---
+++ od_even.py
@@ -0,0 +1,9 @@
+print("hi let's check given number is odd or even ?")
+number = input("enter number")
+number = float(number)
+data = number%2
+if data == 0:
+    print("Number is Even")
+else:
+    print("Number is Odd")
+    
